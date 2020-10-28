ba42fb372944930bfa9fe6de71d88b4d8fb68ee8
Semyon Uchvatov <s-uchvatov@yandex.ru>
2020-10-29 00:11:43 +0300

    api spec proposal

--- src/main/java/tk/laurenfrost/communicator/controller/FoodController.java
+++ src/main/java/tk/laurenfrost/communicator/controller/FoodController.java
@@ -2,10 +2,17 @@ package tk.laurenfrost.communicator.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tk.laurenfrost.communicator.entity.Food;
 import tk.laurenfrost.communicator.service.BoardService;
 import tk.laurenfrost.communicator.service.FoodService;
 
 import java.util.UUID;
+
+// Proposal
+// POST /api/v1/board/{boardId}/food|item
+// POST /api/v1/food
+// DELETE /api/v1/food/{foodId}
+// PATCH /api/v1/food/{foodId}
 
 @RestController
 @RequestMapping(path = "api/v1/item")
@@ -33,6 +40,12 @@ public class FoodController {
     public String deleteFood(@PathVariable String food) {
         this.foodService.deleteFood(UUID.fromString(food));
         return "Food is deleted";
+    }
+
+    @PatchMapping(value = "/{foodId}")
+    public String patchFood(@PathVariable String foodId, @RequestBody Food food) {
+        this.foodService.incrementQuantity(UUID.fromString(foodId));
+        return "Food quantity is incremented";
     }
 
     @PatchMapping(value = "/{food}/inc")
