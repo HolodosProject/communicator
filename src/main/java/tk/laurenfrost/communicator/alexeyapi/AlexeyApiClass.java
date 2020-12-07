d5c1c73bb3cadb01b06ea30a6fa25550c45c5934
meier-th <meier.d.th@gmail.com>
2020-12-07 21:33:13 +0300

    Implement Alexey

--- src/main/java/tk/laurenfrost/communicator/alexeyapi/AlexeyApiClass.java
+++ src/main/java/tk/laurenfrost/communicator/alexeyapi/AlexeyApiClass.java
@@ -1,8 +1,31 @@
 package tk.laurenfrost.communicator.alexeyapi;
 
+import com.google.cloud.speech.v1.*;
+import com.google.protobuf.ByteString;
+
+import java.util.List;
+
 public class AlexeyApiClass {
 
     public static void processSpeech(String boardName, byte[] speechBytes) {
+        System.out.println("Message from "+boardName);
+        try (SpeechClient client = SpeechClient.create()) {
+            ByteString audioBytes = ByteString.copyFrom(speechBytes);
+            RecognitionConfig config = RecognitionConfig.newBuilder()
+                    .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
+                    .setLanguageCode("ru-RU")
+                    .build();
+            RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(audioBytes).build();
+            RecognizeResponse response = client.recognize(config, audio);
+            List<SpeechRecognitionResult> results = response.getResultsList();
+            for (SpeechRecognitionResult result : results) {
+                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                System.out.printf("Transcription: %s%n", alternative.getTranscript());
+            }
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+            error.printStackTrace();
+        }
 
     }
 
--- src/main/java/tk/laurenfrost/communicator/mqtt/MqttSubscriber.java
+++ src/main/java/tk/laurenfrost/communicator/mqtt/MqttSubscriber.java
@@ -4,7 +4,7 @@ import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import tk.laurenfrost.communicator.alexeyapi.AlexeyApiClass;
 
-public class MqttSubscriber implements IMqttMessageListener {
+class MqttSubscriber implements IMqttMessageListener {
 
     @Override
     public void messageArrived(String topic, MqttMessage message) throws Exception {
